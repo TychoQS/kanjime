d0b5ce022daa097ca5148e676678305b60d403b8
TychoQS <tycho.quintana@gmail.com>
2026-05-06 12:36:39 +0100

    Changed playwright to generate video of tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,11 +13,12 @@ export default defineConfig({
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+  preserveOutput: "always",
   use: {
     baseURL: "http://127.0.0.1:4173",
-    trace: "on-first-retry",
-    screenshot: "only-on-failure",
-    video: "retain-on-failure"
+    trace: "retain-on-failure",
+    screenshot: "off",
+    video: "on"
   },
   webServer: {
     command: "npm run preview -- --host 127.0.0.1 --port 4173",

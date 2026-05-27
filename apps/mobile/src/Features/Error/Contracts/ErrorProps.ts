5ccb558d352b5eddd79f566b91e3d5e0cff588f6
TychoQS <tycho.quintana@gmail.com>
2026-05-27 11:29:13 +0100

    Human revision to ErrorProps interface

--- apps/mobile/src/Features/Error/Contracts/ErrorProps.ts
+++ apps/mobile/src/Features/Error/Contracts/ErrorProps.ts
@@ -3,7 +3,7 @@
  *
  * Requirement IDs: R25.
  *
- * @pre An unexpected application error has been captured.
+ * @pre An unexpected application error has been captured during app execution.
  * @inv The visible message does not include stack traces, technical internals, or sensitive details.
  * @post The user sees a clear, non-technical error message.
  */
